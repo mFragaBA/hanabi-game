6ead58564781d7b57a84dd3c717fd5fda13465f2
Martin <fragamartine@gmail.com>
2021-01-16 03:48:01 -0300

    ahora si lo fixea para que sea solo el origin

--- hanabi-sv/flask-api/main.py
+++ hanabi-sv/flask-api/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app,
         engineio_logger=True,
-        cors_allowed_origins='https://mfragaba.github.io/hanabi',
+        cors_allowed_origins='https://mfragaba.github.io',
         async_mode="gevent")
 
 
