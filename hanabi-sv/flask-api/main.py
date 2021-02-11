711fc450f304c5cdadcbc28f5f2061ec95cb934d
Martin <fragamartine@gmail.com>
2021-02-11 04:35:27 -0300

    agrego session y login (por ahora todos usuarios guest

--- hanabi-sv/flask-api/main.py
+++ hanabi-sv/flask-api/main.py
@@ -1,19 +1,32 @@
 import time
-from flask import Flask, render_template, request
+import uuid
+from flask import Flask, render_template, request, session, jsonify
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user
+from flask_session import Session
 from flask_socketio import SocketIO, join_room, leave_room, emit, send
 from model.manager import Manager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 socketio = SocketIO(app,
         logger=True,
         engineio_logger=True,
         cors_allowed_origins=['http://localhost:3000', 'https://mfragaba.github.io'],
         async_mode="eventlet",
-        cookie='hanabi-sid')
+        manage_session=False)
 
 
 gamesManager = Manager()
+
+class User(UserMixin, object):
+    def __init__(self, id=None):
+        self.id = id
+
+@login.user_loader
+def load_user(id):
+    return User(id)
 
 @app.route('/')
 def index():
@@ -22,6 +35,29 @@ def index():
 @app.route('/time')
 def time_api():
     return {'time': time.time()}
+
+@app.route('/session', methods=['GET', 'POST'])
+def connect():
+    if request.method == 'GET':
+        return jsonify({
+            'session': session.get('value', ''),
+            'user': current_user.id
+                if current_user.is_authenticated else 'anonymous' 
+        })
+    
+    data = request.get_json()
+    if 'session' in data:
+        session['value'] = data['session']
+    elif 'user' in data:
+        if data['user']:
+            login_user(User(data['user'])
+        else:
+            logout_user()
+    else:
+        session['value'] = uuid.uuid4()
+        login_user(User('Guest #' + session.get('value', '')))
+    
+    return '', 204
 
 """
 @socketio.on('message')
@@ -44,42 +80,43 @@ def handle_message(arg1, arg2, arg3):
     print('received args:', arg1, arg2, arg3)
 """
 
-nombre_por_sid = {}
-lobby_por_sid = {}
+nombre_por_cliente = {}
+lobby_por_cliente = {}
 
 @socketio.on('disconnect')
 def handle_disconnect():
     #TODO handlear mejor el caso en que la sala se vacíe
     print('+++++++')
-    print("Cliente DESCONECTADO:", request.sid)
+    print("Cliente DESCONECTADO:", current_user.id)
 
-    if request.sid in nombre_por_sid:
-        jugador = (request.sid, nombre_por_sid[request.sid])
+    jugador = (session.get('value', ''), nombre_por_cliente[session.get('value', '')])
 
-        lobby = gamesManager.sala_de(jugador)
-        gamesManager.sacar_jugador(jugador, lobby)
-        leave_room(lobby)
-        socketio.emit('estado_expirado', room=lobby)
-        del nombre_por_sid[request.sid]
+    lobby = gamesManager.sala_de(jugador)
+    gamesManager.sacar_jugador(jugador, lobby)
+    leave_room(lobby)
+    socketio.emit('estado_expirado', room=lobby)
+    del nombre_por_cliente[session.get('value', '')]
 
 @socketio.on('listar_lobbies')
+@authenticated_only
 def handle_listar_lobbies():
     lobbies = gamesManager.listar_lobbies()
     emit('lista_lobbies', lobbies)
 
 @socketio.on('crear_lobby')
+@authenticated_only
 def handle_crear_lobby(data):
     try:
         player_name = data['player_name']
         lobby_name = data['lobby_name']
-        jugador = (request.sid, player_name)
+        jugador = (session.get('value', ''), player_name)
  
 # TODO: arreglar crear_lobby para que use el nombre del jugador así validamos que el nombre sea válido (la otra es que haya un conectar y se encargue de registrar al jugador con dicho nombre)
 #        gamesManager.crear_lobby(lobby_name)
         gamesManager.agregar_jugador(jugador, lobby_name) 
 
-        nombre_por_sid[request.sid] = player_name
-        lobby_por_sid[request.sid] = lobby_name
+        nombre_por_cliente[session.get('value', '')] = player_name
+        lobby_por_cliente[session.get('value', '')] = lobby_name
 
         join_room(lobby_name)
         emit('unido_a_lobby', room=lobby_name)
@@ -87,17 +124,18 @@ def handle_crear_lobby(data):
         emit('error_message', {'error': str(ex)})
 
 @socketio.on('unirse_a_lobby')
+@authenticated_only
 def handle_unirse_a_lobby(data):
     try:
         player_name = data['player_name']
         lobby_name = data['lobby_name']
         
-        jugador = (request.sid, player_name)
+        jugador = (session.get('value', ''), player_name)
 
         gamesManager.agregar_jugador(jugador, lobby_name) 
 
-        nombre_por_sid[request.sid] = player_name
-        lobby_por_sid[request.sid] = lobby_name
+        nombre_por_cliente[session.get('value', '')] = player_name
+        lobby_por_cliente[session.get('value', '')] = lobby_name
 
         join_room(lobby_name)
         emit('unido_a_lobby')
@@ -106,9 +144,10 @@ def handle_unirse_a_lobby(data):
         emit('error_message', {'error': str(ex)})
 
 @socketio.on('iniciar_partida')
+@authenticated_only
 def handle_iniciar_partida():
     try:
-        lobby_name = lobby_por_sid[request.sid]
+        lobby_name = lobby_por_cliente[session.get('value', '')]
         gamesManager.iniciar_juego_en(lobby_name)
 
         socketio.emit('partida_iniciada', room=lobby_name)
@@ -116,26 +155,29 @@ def handle_iniciar_partida():
         emit('error_message', {'error': str(ex)})
 
 @socketio.on('salir_del_lobby')
+@authenticated_only
 def handle_salir_del_lobby(data):
     try:
-        player_name = nombre_por_sid[request.sid]
-        lobby_name = lobby_por_sid[request.sid]
-        jugador = (request.sid, player_name)
+        player_name = nombre_por_cliente[session.get('value', '')]
+        lobby_name = lobby_por_cliente[session.get('value', '')]
+        jugador = (session.get('value', ''), player_name)
         
         gamesManager.sacar_jugador(jugador, lobby_name) 
 
         leave_room(lobby_name)
+        del lobby_por_cliente[session.get('value', '')]
         socketio.emit('lobby_update', gamesManager.estado_del_lobby_de(jugador), room=lobby_name)
 
     except Exception as ex:
         emit('error_message', {'error': str(ex)})
 
 @socketio.on('cortar_partida')
+@authenticated_only
 def handle_cortar_partida(data):
     try:
-        player_name = nombre_por_sid[request.sid]
-        lobby_name = lobby_por_sid[request.sid]
-        jugador = (request.sid, player_name)
+        player_name = nombre_por_cliente[session.get('value', '')]
+        lobby_name = lobby_por_cliente[session.get('value', '')]
+        jugador = (session.get('value', ''), player_name)
         
         gamesManager.cortar_juego_en(lobby_name) 
 
@@ -146,11 +188,12 @@ def handle_cortar_partida(data):
         emit('error_message', {'error': str(ex)})
 
 @socketio.on('registrar_accion')
+@authenticated_only
 def handle_registrar_accion(accion):
     try:
-        player_name = nombre_por_sid[request.sid]
-        lobby_name = lobby_por_sid[request.sid]
-        jugador = (request.sid, player_name)
+        player_name = nombre_por_cliente[session.get('value', '')]
+        lobby_name = lobby_por_cliente[session.get('value', '')]
+        jugador = (session.get('value', ''), player_name)
         
         if 'jugador' not in accion or accion['jugador'] != player_name:
             return
@@ -162,11 +205,12 @@ def handle_registrar_accion(accion):
         emit('error_message', {'error': str(ex)})
 
 @socketio.on('estado_juego_update')
+@authenticated_only
 def handle_actualizar_estado_juego():
     try:
-        player_name = nombre_por_sid[request.sid]
-        lobby_name = lobby_por_sid[request.sid]
-        jugador = (request.sid, player_name)
+        player_name = nombre_por_cliente[session.get('value', '')]
+        lobby_name = lobby_por_cliente[session.get('value', '')]
+        jugador = (session.get('value', ''), player_name)
         
         estado = gamesManager.estado_en_partida_de(jugador)
         
@@ -176,17 +220,19 @@ def handle_actualizar_estado_juego():
         emit('error_message', {'error': str(ex)})
 
 @socketio.on('estado_lobby_update')
+@authenticated_only
 def handle_actualizar_estado_lobby():
     try:
-        player_name = nombre_por_sid[request.sid]
-        lobby_name = lobby_por_sid[request.sid]
-        jugador = (request.sid, player_name)
+        player_name = nombre_por_cliente[session.get('value', '')]
+        lobby_name = lobby_por_cliente[session.get('value', '')]
+        jugador = (session.get('value', ''), player_name)
         
         emit('lobby_update', gamesManager.estado_del_lobby_de(jugador))
     except Exception as ex:
         emit('error_message', {'error': str(ex)})
 
 @socketio.on_error_default
+@authenticated_only
 def default_error_handler(e):
     print("Error", e)
     print(request.event["message"]) # "my error event"
